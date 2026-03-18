22b4ac2ff7eb067e4d2c1bb7945b17faae059e37
toni78702-del <toni78702@gmail.com>
2026-03-17 19:30:58 -0700

    se agrego lo demas

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+
+
+    class Meta:
+        ordering = ('title')
+        verbose_name_plural = 'Categories'
+
+
+    def __str__(self):
+        return self.title
+   
+class Post(models.Model):
+
+
+    ACTIVATE = 'activate'
+    DRAFT = 'draft'
+
+
+    CHOICES_STATUS = {
+        (ACTIVATE, 'activate'),
+        (DRAFT, 'draft')
+    }
+
+
+    category = models.ForeignKey(Category, related_name='posts', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    intro = models.TextField()
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=10, choices=CHOICES_STATUS, default=ACTIVATE)
+    image = models.ImageField(upload_to='upload/', blank=True, null=True)
+
+
+    def __str__(self):
+        return self.title
+   
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='posts', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.name
