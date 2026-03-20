905bd7da94bb085a84373ee229bd02408f0052c9
toni78702-del <toni78702@gmail.com>
2026-03-19 18:16:20 -0700

    la 2da parte

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+
 # Register your models here.
+from .models import Post, Comment, Category
+
+
+admin.site.register(Post)
+admin.site.register(Category)
+admin.site.register(Comment)
+
