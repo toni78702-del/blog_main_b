8803e4000fe40809d28f6860338f3e1ad0b0c71a
toni78702-del <toni78702@gmail.com>
2026-03-24 19:41:38 -0700

    se agrego cambios

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# -*- coding: utf-8 -*-
+
+
+from django.db.models import Q
+from django.shortcuts import render, get_object_or_404, render
+
+
+from .models import Post, Category
+
+
+# Create your views here.
+def home(request):
+    posts = Post.objects.filter(status=Post.ACTIVATE).order_by('-created_at')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+
